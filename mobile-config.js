67c40fc43a4506e8a9e667748154640244f7223f
Leoss <gael.flores@gmail.com>
2016-01-26 19:56:14 +0700

    added file

---
+++ mobile-config.js
@@ -0,0 +1,12 @@
+App.info({
+  name: 'Snowball',
+  description: "Snowball is a meritocratic platform to raise the best ideas for change",
+  version: '0.0.1'
+});
+
+App.icons({
+  'android_ldpi': 'public/images/logo.png',
+  'android_mdpi': 'public/images/logo.png',
+  'android_hdpi': 'public/images/logo.png',
+  'android_xhdpi': 'public/images/logo.png'
+});
