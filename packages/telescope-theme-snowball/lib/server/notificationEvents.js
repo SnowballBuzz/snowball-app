9e1cf5165d19c797f0df2a7348c4304a7a658e32
Julian Kingman <julian@designman.io>
2016-03-14 18:31:59 -0400

    added private upvotes notification

--- packages/telescope-theme-snowball/lib/server/notificationEvents.js
+++ packages/telescope-theme-snowball/lib/server/notificationEvents.js
@@ -1,12 +1,16 @@
 Posts.after.update(function (userId, post, fieldNames, modifier, options) {
     //console.log('updated', userId, post, fieldNames, modifier, options);
     if (_.contains(fieldNames, 'upvotes')) {
-      console.log('upvoted');
       var notifiedUserIds = _.pluck(Users.find({'telescope.notifications.posts': true}, {fields: {_id: 1}}).fetch(), '_id');
-      var notificationData = {post: _.pick(post, '_id', 'userId', 'title', 'url', 'upvotes')};
       notifiedUserIds = _.intersection(notifiedUserIds, post.userId);
-      if (_.contains([2, 5, 10, 50, 100, 200, 500], post.upvotes)) {
+      var groupUserIds = _.intersection(Users.find({}))
+      var notificationData = {post: _.pick(post, '_id', 'userId', 'title', 'url', 'upvotes')};
+      var group = Categories.findOne(post.categories[0]);
+      if (_.contains([2, 5, 10, 50, 100, 200, 500], post.upvotes) && !group.isPrivate) {
         console.log('2,5,10,50,100,200, or 500');
+        Herald.createNotification(notifiedUserIds, {courier: 'xPosts', data: notificationData});
+      } else if (post.upvotes === 5 && group.isPrivate){
+        console.log('two private upvotes');
         Herald.createNotification(notifiedUserIds, {courier: 'xPosts', data: notificationData});
       }
     }
