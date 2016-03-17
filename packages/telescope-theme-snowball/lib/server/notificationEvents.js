ad513f2dcb97a50c3a385469bb2bf8bb7a159602
Julian Kingman <julian@designman.io>
2016-03-17 17:15:38 -0400

    autosave ationale

--- packages/telescope-theme-snowball/lib/server/notificationEvents.js
+++ packages/telescope-theme-snowball/lib/server/notificationEvents.js
@@ -1,5 +1,6 @@
 Posts.after.update(function (userId, post, fieldNames, modifier, options) {
-    //console.log('updated', userId, post, fieldNames, modifier, options);
+    // console.log('updated', userId, post, fieldNames, modifier, options);
+    // console.log('post updated');
     if (_.contains(fieldNames, 'upvotes')) {
       var notifiedUserIds = _.pluck(Users.find({'telescope.doNotify': true}, {fields: {_id: 1}}).fetch(), '_id');
       notifiedUserIds = _.intersection(notifiedUserIds, [post.userId]);
@@ -14,11 +15,23 @@ Posts.after.update(function (userId, post, fieldNames, modifier, options) {
         console.log('5 private upvotes', notifiedUserIds, notificationData);
         Herald.createNotification(notifiedUserIds, {courier: 'xPosts', data: notificationData});
       }
+
+    }
+    if (typeof modifier.$inc !== 'undefined' && (modifier.$inc.upvotes || modifier.$inc.downvotes)) {
+      //todo: points for upvoting
+      // console.log('someone voted', userId, 'post author', post.userId);
+      Users.update(userId, {$inc: {'telescope.karma': 1}});
+      if (userId && modifier.$inc.upvotes > 0) {
+        Users.update(post.userId, {$inc: {'telescope.karma': 2}});
+      }
+    }
+    if (typeof modifier.$inc !== 'undefined' && modifier.$inc.shares) {
+      Users.update(userId, {$inc: {'telescope.karma': 4}});
     }
   }
 );
 
 Posts.after.insert(function (userId, post, fieldNames) {
   // var user = Users.findOne(post.userId);
-  Users.update(userId, {$inc: {'telescope.karma': 2}});
+  Users.update(userId, {$inc: {'telescope.karma': 5}});
 });
